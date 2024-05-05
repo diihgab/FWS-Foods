6a9794a4e0714ba3bc97ea1a942aa5a2a53eab4e
diihgab <dihgab20@gmail.com>
2024-05-05 19:48:25 -0400

    feat: add product list and product item components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 const Home = () => {
   return (
@@ -15,8 +18,33 @@ const Home = () => {
         <CategoryList />
       </div>
       <div className="px-5 pt-6">
+        _
         <Image
           src="/promo-banner-01.png"
+          alt="Até 30% de Desconto"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button variant="ghost" className="text-primary hover:bg-transparent">
+            Ver Todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
+      </div>
+
+      <div className="px-5 pt-6">
+        _
+        <Image
+          src="/promo-banner-02.png"
           alt="Até 30% de Desconto"
           height={0}
           width={0}
